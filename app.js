8ff75e28d782656e974fcd9e328373f89b7f7278
CLASHERBROs <parigames@gmail.com>
2020-05-31 12:25:05 +0530

    Added custom module and ejs layouts

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 // jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require( __dirname + "/date.js");
+var day = date.getDay();
 
+var items = [];
+let workItems = [];
 const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 app.set('view engine','ejs');
-var day;
+app.get("/about",function(req,res){
+    res.render("about");
+});
 app.get("/",function(req,res){
-   var today = new Date();
-switch(today.getDay())
-{case 1: day = "Monday";
-    break;
-case 2: day = "Tuesday";
-    break;
-case 3:day = "Wednesday";
-    break;
-case 4:day = "Thursday";
-    break;
-case 5:day = "Friday";
-    break;
-case 6:day = "Saturday";
-    break;
-case 0:day = "Sunday";
-    break;
 
-}
-   res.render("list",{kindOfDay:day});
+
+   res.render("list",{listTitle:day,newListItems:items});
 
 });
+app.post("/",function (req,res) {
+    if(req.body.list == "work"){
+      workItems.push(req.body.toDoItem) ;
+        res.redirect("/work");
+    }
+    else{
+    items.push(req.body.toDoItem) ;
+    res.redirect("/");
+    }
+  });
+  app.get("/work",function(req,res){
+
+   res.render("list",{listTitle:"work",newListItems:workItems});
+  });
+  
+  app.post("/work",function (req,res) {
+    workItems.push(req.body.toDoItem) ;
+    res.redirect("/work");
+
+  });
 
 
 app.listen(3000,function(){
